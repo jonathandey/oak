4f56817c1eb34098ecefde027f514382170b47c5
Jonathan Dey <jon@trenttech.co.uk>
2021-11-05 14:57:34 +0000

    Initial commit

---
+++ src/sshit.cr
@@ -0,0 +1,37 @@
+require "json"
+require "time"
+require "kemal"
+require "tallboy"
+
+data_rows = Array(Array(Time | String)).new
+sockets = Hash(UInt64, HTTP::WebSocket).new
+
+# TODO: Write documentation for `Sshit`
+module Sshit
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+  get "/" do |env|
+    "ok"
+  end
+
+  post "/" do |env|
+    uuid = env.params.json["uuid"]
+    payloads = env.params.json["payloads"]
+
+    sockets.each do |hash, socket|
+      unless socket.closed?
+        socket.send(env.params.json.to_json.to_s)
+      end
+    end
+
+    "ok"
+  end
+
+  ws "/" do |socket|
+    puts socket.hash
+    sockets[socket.hash] = socket
+  end
+
+  Kemal.run(23517)
+end
